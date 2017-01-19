56d970796bac1b13200fd40c4d1878d279725ae6
Nur Wahyudi <nur.wahyudi93@yahoo.com>
2017-01-20 02:27:59 +0700

    file tes baru banget dan fix

--- tes.php
+++ tes.php
@@ -5,8 +5,19 @@
 
 </head>
 <body>
+<!-- Mengunakan Function -->
+	 
+	 <script type="text/javascript">
+	 	function background() {
+	 		document.bgColor="red";
+	 	}
 
-	<p>Ini Tes 1
+	 	function pesan(isipesan) {
+	 		alert (isipesan);
+	 	}
+	 </script>
+	
+	<!-- <p>Ini Tes 1
 	<script type="text/javascript">
 		alert ("ini Tes 1");
 	</script>
@@ -16,6 +27,20 @@
 			<script type="text/javascript">
 		document.bgColor="red";
 	</script>
+	</p> -->
+
+	<p>Ini Tes 1
+	<script type="text/javascript">
+		pesan ("Cobalah");
+	</script>
 	</p>
+
+	<p>Ini Tes 2
+			<script type="text/javascript">
+		background ();
+	</script>
+	</p>
+
+
 </body>
 </html>
